2c5ef705e09934c9eb8c4e893ada8bf5cbfc0c6b
MickMake <github@mickmake.com>
2020-03-17 10:05:45 +1100

    updated

--- 0.62.2/build/unit-tests/hugo/02-release.sh
+++ 0.62.2/build/unit-tests/hugo/02-release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Created on 2020-03-17T10:03:58+1100, using template:02-release.sh.tmpl and json:gearbox.json
+# Created on 2020-03-17T10:05:34+1100, using template:02-release.sh.tmpl and json:gearbox.json
 
 p_info "hugo-0.62.2" "Release test started."
 
