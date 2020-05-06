5ca23d0367f17b8e6e7a2e690728bea84cce5cac
MickMake <github@mickmake.com>
2020-05-06 18:48:40 +1000

    Added newer versions.

---
+++ versions/0.68.3/build/unit-tests/hugo/02-release.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Created on 2020-05-06T18:48:09+1000, using template:02-release.sh.tmpl and json:gearbox.json
+
+p_info "hugo-0.68.3" "Release test started."
+
+ps -eaf
+
+p_info "hugo-0.68.3" "Release test finished."
---
+++ versions/0.69.2/build/unit-tests/hugo/02-release.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Created on 2020-05-06T18:48:10+1000, using template:02-release.sh.tmpl and json:gearbox.json
+
+p_info "hugo-0.69.2" "Release test started."
+
+ps -eaf
+
+p_info "hugo-0.69.2" "Release test finished."
